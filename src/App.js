ddd4ce5d539b48a8ec6b505fdaeb0ad7a5adbd31
Robert Apelian <robertmapelian@gmail.com>
2020-04-27 16:06:27 -0400

    finalized cra version - shop instead of games

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ const pages = [
   {
     key: 0,
     title: 'art',
-    image: Thumbnails.ArtThumb
+    image: Thumbnails.ArtThumb,
+    url: 'https://www.instagram.com/robertapelian/'
   },
   {
     key: 1,
-    title: 'games',
-    image: Thumbnails.GamesThumb
+    title: 'shop',
+    image: Thumbnails.ShopThumb,
+    url:  'https://redbubble.com/people/robapelian/shop'
   },
   {
     key: 2,
     title: 'videos',
-    image: Thumbnails.VideoThumb
+    image: Thumbnails.VideoThumb,
+    url: 'https://vimeo.com/user6798736'
   },
   {
     key: 3,
@@ -30,8 +33,7 @@ const pages = [
 class App extends Component {
   render() {
     return (
-      <div style={{backgroundColor: '#343a34', color: 'rgb(255,255,255)', height: '100%'}}>
-        {/*<div style={{display: 'flex', justifyContent: 'center', fontSize: 64, marginBottom: '16px'}}><i>rob apelian</i> </div>*/}
+      <div style={{backgroundColor: '#343a34', color: 'rgb(255,255,255)', height: '100%', fontFamily: 'vulf-mono'}}>
         <Main pages={pages}/>
       </div>
     )
--- src/images/images.js
+++ src/images/images.js
@@ -1,11 +1,11 @@
 import ArtThumb from './art.png'
 import VideoThumb from './video.png'
 import WorkThumb from './work.png'
-import GamesThumb from './games.png'
+import ShopThumb from './shop.png'
 
 export default {
 	ArtThumb,
 	VideoThumb,
 	WorkThumb,
-	GamesThumb
+	ShopThumb
 }
