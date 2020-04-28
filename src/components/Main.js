2355b768365cfb51ae2330f4ab1bd2b9a8e57d72
Robert Apelian <robertmapelian@gmail.com>
2020-04-28 13:41:11 -0400

    iiiiiiiiii can give you gaaatsubeeee

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import PageTile from './PageTile'
 
-export default class Main extends Component {
-	render() {
-  		return <div style={{display: 'flex', width: '100vw', flexWrap: 'wrap'}}>
-    	{
-      		this.props.pages.map(page => <PageTile page={page}/>)
-    	}
-  		</div>
+const Main = (props) => {
+	return <div style={{display: 'flex', width: '100vw', flexWrap: 'wrap'}}>
+	{
+  		props.pages.map(page => <PageTile page={page}/>)
 	}
+	</div>
 }
+
+export default Main;
